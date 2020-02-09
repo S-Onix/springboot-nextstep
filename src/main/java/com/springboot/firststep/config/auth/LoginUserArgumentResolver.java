f373a06dc8d72df235e535e152692d69b2f81451
S-Onix <msyoo101@hansung.ac.kr>
2020-02-09 20:52:03 +0900

    네이버 로그인 API 연동 성공 및 OAuth2 테스트 케이스 코드 성공

--- src/main/java/com/springboot/firststep/config/auth/LoginUserArgumentResolver.java
+++ src/main/java/com/springboot/firststep/config/auth/LoginUserArgumentResolver.java
@@ -18,10 +18,10 @@ public class LoginUserArgumentResolver implements HandlerMethodArgumentResolver 
 
     @Override
     public boolean supportsParameter(MethodParameter parameter) { // 컨트롤러의 메소드가 특정 파라미터를 지원하는지 확인함 (@LoginUser 이면서 SessionUser.class가 true 인 경우를 말한다.)
-        boolean isLoginUserAnnotaion = parameter.getParameterAnnotation(LoginUser.class) != null ;
+        boolean isLoginUserAnnotation = parameter.getParameterAnnotation(LoginUser.class) != null;
         boolean isUserClass = SessionUser.class.equals(parameter.getParameterType());
 
-        return isLoginUserAnnotaion && isUserClass;
+        return isLoginUserAnnotation && isUserClass;
     }
 
     // 컨트롤러 메소드 파라미터에 전달될 객체 > 여기서는 Session에 대한 정보를 넘겨줌
--- src/main/java/com/springboot/firststep/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/springboot/firststep/config/auth/dto/OAuthAttributes.java
@@ -25,6 +25,11 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        // 로그인에 연결할 API들에 대한 정보들을 추가 입력해주면 됨
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
+
         return ofGoogle(userNameAttributeName, attributes);
     }
 
@@ -34,6 +39,19 @@ public class OAuthAttributes {
                 .email((String) attributes.get("email"))
                 .picture((String) attributes.get("picture"))
                 .attributes(attributes)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes){
+        // application-oauth에 설정된 내용(Naver 로그인에 필요한 내용)을 가져옴 >> OauthAttributes에 설정을 세팅해줌
+        Map<String, Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profileImage"))
+                .attributes(response)
                 .nameAttributeKey(userNameAttributeName)
                 .build();
     }
